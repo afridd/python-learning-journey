7d65409a4ab0c334433c0eeeefa4a27b91188308
afridd <afridashrafali@gmail.com>
2026-08-21 23:36:09 +0530

    Wrote a codefor class Cat

---
+++ cat.py
@@ -0,0 +1,18 @@
+class Cat:
+    """A simple attempt to make a cat."""
+
+    def  __init__(self, name, age):
+        """Initialize name and age attributes.""" 
+        self.name = name 
+        self.age = age
+
+    def sit(self):
+        """Simulate a cat sitting in respond to a command."""
+        print(f"{self.name} is now sitting.")
+
+    def roll_over(self)  :
+        """simulate a cat rolling over in response to a command."""
+        print(f"{self.name} rolled over!")  
+
+            
+
