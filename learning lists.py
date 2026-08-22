d796e4b0f01b71a5f7aba5b30dc731e3f75b2262
afridd <afridashrafali@gmail.com>
2026-08-22 16:24:51 +0530

    LEarning lists and doing list operation

---
+++ learning lists.py
@@ -0,0 +1,9 @@
+# Storing names of my friends in a list and printing one by one
+friends = ['Afsar', 'Khalid', 'Hadil', 'Yasar', 'Zuhair', 'Imthiyas', 'Rizwan', 'Uwais']
+for friend in friends:
+    print(friend)
+
+# Sending a greeting to them
+for friend in friends:
+    print(f"Hello {friend}, How are you?")
+    
