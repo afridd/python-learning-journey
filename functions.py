090cd5ca3b3117a7c301dee0911a04738ef7eb57
afridd <afridashrafali@gmail.com>
2026-08-23 11:10:29 +0530

    Learning positional, keyword and default arguments

--- functions.py
+++ functions.py
@@ -4,3 +4,23 @@ def greet_user(username):
     print(f"Hello, {username.title()}!")
 
 greet_user("rose")
+
+#Positional arguments
+def describe_pet(animal_type, pet_name):
+    """Diplaying information about pet."""
+    print(f"I have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.\n")
+
+describe_pet('parrot','zazu')
+describe_pet('cat','dairy milk')
+
+#Keyword arguments
+describe_pet(pet_name='Jerry',animal_type='Mouse')
+
+#default arguments
+def describe_pet(animal_type, pet_name='Tom'):
+    """Diplaying information about pet."""
+    print(f"I have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.\n")
+    
+describe_pet('cat')
