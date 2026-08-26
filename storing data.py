f7b6a508eabe42aa2c53b5f675eb93359c665f2f
afridd <afridashrafali@gmail.com>
2026-08-26 16:22:07 +0530

    Use json.dumps to store a list

---
+++ storing data.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+import json #Use json to store(dump) and retrieve(load) data
+
+numbers = [2, 3, 5, 7, 11, 13]
+path = Path("python-learning-journey/numbers.json")
+contents = json.dumps(numbers)
+path.write_text(contents)
