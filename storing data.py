9b130ef94bd322f52a8009ae764eb15529b7d117
Afrid <afridashrafali@gmail.com>
2026-08-26 19:44:45 +0530

    Fix comment for data storage in storing data.py
    Corrected comment to use 'data' in plural form.

--- storing data.py
+++ storing data.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import json #Use json to store(dump) and retrieve(load) data
+import json #Use json to store(dump) and retrieve(load) datas
 
 numbers = [2, 3, 5, 7, 11, 13]
 path = Path("python-learning-journey/numbers.json")
