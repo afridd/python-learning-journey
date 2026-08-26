91bf2e1776084da0e190c8bf62d0c7a20c3b6387
afridd <afridashrafali@gmail.com>
2026-08-26 16:44:46 +0530

    Stored and loaded username

--- loading stored data.py
+++ loading stored data.py
@@ -6,3 +6,10 @@ contents =path.read_text()
 numbers = json.loads(contents)
 
 print(numbers)
+
+
+location = Path("python-learning-journey/usernames.json")
+values = location.read_text()
+username = json.loads(values)
+
+print(f"Welcome back, {username}!")
--- storing data.py
+++ storing data.py
@@ -5,3 +5,10 @@ numbers = [2, 3, 5, 7, 11, 13]
 path = Path("python-learning-journey/numbers.json")
 contents = json.dumps(numbers)
 path.write_text(contents)
+
+username = input("What is your name? ")
+location = Path("python-learning-journey/usernames.json")
+values = json.dumps(username)
+location.write_text(values)
+
+print(f"We'll remember youu when you come back, {username}!")
